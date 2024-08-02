b6662397ff9ea4c051b8391470d421ac3f1224a5
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2024-08-02 16:40:42 -0700

    global.R adjustments

--- global.R
+++ global.R
@@ -4,83 +4,113 @@ install.packages("SpaDES.project", repos = repos)
 
 library(SpaDES.project)
 
-out <- setupProject(
+out <- SpaDES.project::setupProject(
   name = "BGCprojections",
   paths = list("inputPath" = "data/",
                "outputPath" = "outputs/",
                "modulePath" = "modules/",
-               "packagePath" = pkgPath, 
+               "packagePath" = "packages/", 
                "cachePath" = "reproducible.cache/",
                "projectPath" = "."),
   options = list("reproducible.cachePath" = "reproducible.cache/",
                  "reproducible.destinationPath" = paths$inputPath,
-                 "climr.cache.path" = "climr.cache/"),
+                 "reproducible.useMemoise" = TRUE,
+                 "climr.cache.path" = "climr.cache/",
+                 "repos" = repos),
   ## install, but don't load these:
   packages = c("future", 
-               "PredictiveEcology/reproducible@modsForLargeArchives",
-               "sf"), 
+               "sf",
+               "CeresBarros/climr@devl"), ## override
   modules = c("bcgov/BGCprojections@main/modules/BGCWNAmodel_trainAndProj"),
-  Restart = FALSE,
+  overwrite = FALSE,
+  Restart = TRUE,
   times = list(start = 1, end = 1), 
-  studyArea =  {
+  ## reduce things for testing
+  params = list(
+    BGCWNAmodel_trainAndProj = list(
+      "GCMperiods" = climr::list_gcm_periods()[1:2],
+      "GCMruns" = 1,
+      "GCMs" = climr::list_gcms()[c(1, 4, 5)],
+      "SSPs" = climr::list_ssps()[2:4],
+      "nthread_mlr3" = list("lrn_rf_resp" = 3)
+    )
+  ),
+  studyArea = {
     # terra::vect(terra::ext(c(-125, -112, 43, 55)), crs = "EPSG:4326")  ## not enough mem with 32Gb
     terra::vect(terra::ext(c(-122, -118, 46, 50)), crs = "EPSG:4326") ## south BC, w/ US
   },
   studyArea_predict = {
     studyArea
-  }
-)
-
-BGCprojections_simInit <- do.call(SpaDES.core::simInit, out) |>
-  reproducible::Cache(userTags = "simInit")
-# Mar15 11:09:53 simInit Global parameter(s) not used in any module: .studyAreaName.
-# Elpsed time for simInit: 1.321427 mins
-# Saving large object (fn: SpaDES.core::simInit, cacheId: a44bf279ada6880e) to Cache: 1.1 Gb Done!
-#   There were 26 warnings (use warnings() to see them)
-# > Sys.time()
-# [1] "2024-03-15 11:17:09 PDT"
-
-BGCprojections_simOut <- SpaDES.core::spades(BGCprojections_simInit)
-
-# ## install/load packages
-# Require::Require(c(
-#   "bcgov/climr@devl (HEAD)", 
-#   "data.table",
-#   "foreach",
-#   "ggplot2", 
-#   "reproducible",
-#   # "smotefamily", 
-#   "mlr3learners",
-#   "mlr3spatial", 
-#   "mlr3spatiotempcv",
-#   "mlr3viz",
-#   "SpaDES.core", 
-#   "terra", 
-#   "themis",
-#   "tidymodels"
-# ))
-
-## install, but don't load these.
-# Require::Require(c(
-#   "future",
-#   "sf"), 
-#   require = FALSE)
-
-## in 03_RunPredictHex -- not sure what we'll need
-# require(foreach)
-# require(dplyr)
-# require(reshape2)
-# library(doParallel)
-# library(tidyr)
-# require(sf)
-# require(RPostgreSQL)
-# library(disk.frame)
-# require(RPostgres)
-
-
-
-
-## source scripts
-source("SpaDES/R/02_Build_WNA_BGC_trainingset.R")
-source("SpaDES/R/03_RunPredictHex.R")
-source("SpaDES/R/04_CreateBGCfutsMap.R")  
+  },
+  modelsToTrain = {
+    list(
+      "lrn_rf_resp" = quote(mlr3::lrn("classif.ranger",
+                                      predict_type = "response",
+                                      num.trees = 501,
+                                      splitrule =  "extratrees",
+                                      mtry = 4,
+                                      min.node.size = 2,
+                                      importance = "permutation",
+                                      write.forest = TRUE))
+    )
+  },
+  validStrategies = {
+    list(
+      "lrn_rf_resp" = quote(mlr3::rsmp("repeated_spcv_coords", folds = 10, repeats = 1))
+    ) 
+  },
+  
+  BGCprojections_simInit <- SpaDES.core::simInit2(out)
+  
+  # unloadNamespace("ccissr")
+  # unloadNamespace("climr")
+  # devtools::load_all("C:/Users/cbarros/GitHub/BCGov/climr/")
+  # devtools::load_all("C:/Users/cbarros/GitHub/BCGov/ccissr/")
+  # 
+  # SpaDES.core::restartSpades()
+  
+  BGCprojections_simOut <- SpaDES.core::spades(BGCprojections_simInit)
+  
+  # ## install/load packages
+  # Require::Require(c(
+  #   "bcgov/climr@devl (HEAD)", 
+  #   "data.table",
+  #   "foreach",
+  #   "ggplot2", 
+  #   "reproducible",
+  #   # "smotefamily", 
+  #   "mlr3learners",
+  #   "mlr3spatial", 
+  #   "mlr3spatiotempcv",
+  #   "mlr3viz",
+  #   "SpaDES.core", 
+  #   "terra", 
+  #   "themis",
+  #   "tidymodels"
+  # ))
+  
+  ## install, but don't load these.
+  # Require::Require(c(
+  #   "future",
+  #   "sf"), 
+  #   require = FALSE)
+  
+  ## in 03_RunPredictHex -- not sure what we'll need
+  # require(foreach)
+  # require(dplyr)
+  # require(reshape2)
+  # library(doParallel)
+  # library(tidyr)
+  # require(sf)
+  # require(RPostgreSQL)
+  # library(disk.frame)
+  # require(RPostgres)
+  
+  
+  
+  
+  ## source scripts
+  source("SpaDES/R/02_Build_WNA_BGC_trainingset.R")
+  source("SpaDES/R/03_RunPredictHex.R")
+  source("SpaDES/R/04_CreateBGCfutsMap.R")  
+  
