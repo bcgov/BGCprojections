a3e6c6967c7c06fb14318a08722a66b55f3c7c0e
Courtney Collins <Courtney.Collins@gov.bc.ca>
2024-11-15 18:03:22 -0800

    filtering BGC model training data script

--- R/create_training_set.R
+++ R/create_training_set.R
@@ -19,13 +19,13 @@ library(sf)
 library(raster)
 library(tidyverse)
 
-#pull in training pts from Qgis-----
-#200 pts per BGC- Will M. 11/14/24
+#Generate climate attributed training data-----
+#pull in training pts from Qgis- 200 pts per BGC
 trainpts<- read.csv("spatialdata/WNA_v13_200_rndpts_15Nov.csv")
-  
-##pull in climate data for training pts---- 
+trainpts$id<-rownames(trainpts)
+
+##pull in climate data for training pts
 my_grid<-trainpts
-my_grid$id<-rownames(my_grid)
 my_grid<-dplyr::select(my_grid, -BGC)
 colnames(my_grid) <- c( "lon", "lat", "elev", "id") # rename column names to what climr expects
 
@@ -34,23 +34,26 @@ varsl = c("Tmax","Tmin", "PPT")
 ## climr call- This will return the observed 1961-1990 climates for the raster grid points.
 cache_clear()
 gc()
-climlayer <- downscale(
-  xyz = my_grid,  which_refmap = "refmap_climr",
-  #obs_periods = "2001_2020", 
-  vars = varsl)
 
-save(climlayer, file="trainingpts_w_clim.Rdata")
+#only use if need to re-run climr 
+#climlayer <- downscale(
+#  xyz = my_grid,  which_refmap = "refmap_climr",
+#  obs_periods = "2001_2020", 
+#  vars = varsl)
 
-#assess climate variability within BGCs
+#save(climlayer, file="trainingpts_w_clim.Rdata")
+
+#assess climate variability within BGCs----
 #merge back with BGC info 
-load(file="trainingpts_w_clim.Rdata")
-climlayer<-left_join(climlayer, my_grid)
-climlayer<-left_join(climlayer, trainpts)
+#load(file="trainingpts_w_clim.Rdata")
+#climlayer<-left_join(climlayer, my_grid)
+#climlayer<-left_join(climlayer, trainpts)
 #check that elev matches and join worked 
-plot(climlayer$Elev1, climlayer$elev) #good 
-climlayer<-dplyr::select(climlayer, -Elev1, -xcoord, -ycoord)
+#plot(climlayer$Elev1, climlayer$elev) #good 
+#climlayer<-dplyr::select(climlayer, -Elev1, -xcoord, -ycoord)
 
-save(climlayer, file="trainingpts_w_climALL.Rdata")
+#save again
+#save(climlayer, file="trainingpts_w_clim.Rdata")
 
 rm(my_grid)
 rm(trainpts)
@@ -58,53 +61,89 @@ gc()
 
 #look at data
 library(ggplot2)
+load(file="trainingpts_w_clim.Rdata")
 
 ggplot(climlayer, aes(x=BGC, y=Tmin))+ geom_boxplot()
 ggplot(climlayer, aes(x=BGC, y=Tmax))+ geom_boxplot()
 ggplot(climlayer, aes(x=BGC, y=PPT))+ geom_boxplot()
 
 
-#generate estimates by BGC
-min(climlayer$Tmin)
+#generate estimates of climate variation by BGC
+min(climlayer$Tmin)#add 15 to all temps so non negative, non zero CVs 
 CVs<-group_by(climlayer, BGC)%>%summarise(avgTmin=mean(Tmin+15), avgTmax=mean(Tmax+15), avgPpt=mean(PPT),
                                           sdTmin=sd(Tmin+15), sdTmax=sd(Tmax+15), sdPpt=sd(PPT))%>%mutate(cvTmin=sdTmin/(avgTmin), 
                                                                                                     cvTmax=sdTmax/(avgTmax), 
                                                                                                      cvPpt=sdPpt/avgPpt)
 CVlong<-pivot_longer(CVs, cols = c(cvTmin, cvTmax, cvPpt), names_to = 'climvar', values_to = 'cv')%>%distinct(.)
 
-ggplot(CVlong, aes(x=BGC, y=cv))+ geom_point()+ facet_wrap(~climvar,scales = 'free')+ geom_hline(yintercept = 0.3)
+#plot all CVs
+cv1<-ggplot(CVlong, aes(x=BGC, y=cv))+ geom_point()+ facet_wrap(~climvar,scales = 'free')+ geom_hline(yintercept = 0.3)
 
+#pull in BGC areas to scale
+BGCareas<-read.csv('spatialdata/WNA_v13_BGC_area.csv')
+min(BGCareas$area)
+#rescale areas by smallest 
+BGCareas$area2<-BGCareas$area/min(BGCareas$area)
+min(BGCareas$area2)#now =1
+max(BGCareas$area2)
 
-#exclusion criteria
-BGC_exclude<-subset(CVs, cvPpt>0.3)
-BGC_exclude<-BGC_exclude$BGC
+CVs<-left_join(CVs, BGCareas)
+CVs<-mutate(CVs, cvTmin_scaled=(cvTmin/area2)*100, 
+            cvTmax_scaled=(cvTmax/area2)*100, 
+            cvPpt_scaled=(cvPpt/area2)*100)
 
+CVlong2<-pivot_longer(CVs, cols = c(cvTmin_scaled, cvTmax_scaled, cvPpt_scaled), names_to = 'climvar', values_to = 'cv_scaled')%>%distinct(.)
+
+cv2<-ggplot(CVlong2, aes(x=BGC, y=cv_scaled))+ geom_point()+ facet_wrap(~climvar,scales = 'free')+ geom_hline(yintercept = 0.002)+
+ylim(0,0.02)
+
+gridExtra::grid.arrange(cv1, cv2)
+
+#exclusion criteria- scaled CV> 0.003
+BGC_exclude<-subset(CVlong2, cv_scaled>=0.003)
+BGC_exclude<-unique(BGC_exclude$BGC) #25 BGCs - all relatively small/ transitional units  - most common ESSF, MH & IDF 
+ 
+climlayer<-left_join(climlayer, CVs)
 climlayer<-mutate(climlayer, exclude=if_else(BGC %in% BGC_exclude, 'Y', 'N'))
 
-ggplot(subset(climlayer,exclude=='Y'), aes(x=BGC, y=Tmin, fill=cvPpt, colour = cvPpt))+ geom_boxplot()+
+ggplot(subset(climlayer,exclude=='Y'), aes(x=BGC, y=Tmin, fill=cvPpt_scaled, colour = cvPpt))+ geom_boxplot()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggplot(subset(climlayer,exclude=='Y'), aes(x=BGC, y=Tmax))+ geom_boxplot()+
+ggplot(subset(climlayer,exclude=='Y'), aes(x=BGC, y=Tmax, fill=cvPpt_scaled, colour = cvPpt))+ geom_boxplot()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggplot(subset(climlayer,exclude=='Y'|BGC=="CWHxs"|BGC=="IDFww"), aes(x=BGC, y=PPT,fill=cvPpt, colour = cvPpt ))+ geom_boxplot()+
+ggplot(subset(climlayer,exclude=='Y'), aes(x=BGC, y=log(PPT),fill=cvPpt_scaled, colour = cvPpt ))+ geom_boxplot()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-#add in IDFww and CWHxs for comparison
-#ggplot(subset(climlayer,exclude=='Y'|BGC=="CWHxs"|BGC=="IDFww"), aes(x=BGC, y=Tmin,fill=cvTmin, colour = cvTmin ))+ geom_boxplot()+
-#  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-#ggplot(subset(climlayer,exclude=='Y'|BGC=="CWHxs"|BGC=="IDFww"), aes(x=BGC, y=Tmax,fill=cvTmax, colour = cvTmax ))+ geom_boxplot()+
-#  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-#ggplot(subset(climlayer,BGC=="IDFww"|BGC=="CWHxs"), aes(x=BGC, y=PPT,fill=cvPpt, colour = cvPpt ))+ geom_boxplot()+
-#  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+climlayer_filt<-subset(climlayer, exclude=="N")#72224 obs
+unique(climlayer_filt$BGC) #370 BGCs- removed 25
 
-#remove actual outliers by BGC
-climlayer<-mutate(climlayer, remove= case_when(BGC=="BAFAun"& PPT>=3000~'Y',
-                                               BGC=="CCHun_CA"& PPT>=1000~'Y',
-                                               BGC=="CDFxm_CA"& PPT>=1500~'Y',
-                                               BGC=="CMAun"& PPT>=5700~'Y',
-                                               BGC=="CMXdm_OR"& PPT>=1800~'Y',
-                                               BGC=="CMXmm_OR"& PPT>=1800~'Y',
-                                               
-                                               ))
+#remove actual outlier training points from remaining BGCs > +/- 3 SDevs from mean 
+ggplot(climlayer_filt, aes(x=BGC, y=Tmin, fill=cvPpt_scaled, colour = cvPpt))+ geom_boxplot()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+climlayer_filt<-group_by(climlayer_filt, BGC)%>%mutate(avgTmin=mean(Tmin), avgTmax=mean(Tmax), avgPpt=mean(PPT),
+                                          sdTmin=sd(Tmin), sdTmax=sd(Tmax), sdPpt=sd(PPT))%>%
+  mutate(sdevsTmin=(Tmin-avgTmin)/sdTmin, 
+         sdevsTmax=(Tmax-avgTmax)/sdTmax, 
+         sdevsPPT=(PPT-avgPpt)/sdPpt) %>%subset(.,  sdevsPPT>-3 &  sdevsPPT<3) %>%subset(., sdevsTmin>-3 & sdevsTmin<3)%>%
+         subset(., sdevsTmax>-3 & sdevsTmax<3)
+
+#71262 obs - 962 outliers removed 
+
+ggplot(climlayer_filt, aes(x=BGC, y=Tmin, fill=cvPpt_scaled, colour = cvPpt))+ geom_boxplot()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+#make sure no BGCs are highly under sampled- min n=50
+BGCpts<-group_by(climlayer_filt, BGC)%>%summarise(ct=n())
+BGCkeep<-filter(BGCpts, ct>50)#removed 3 BGCs- IDFdk_WA,MSxx_NV,MSxk_WA
+BGCkeep<-unique(BGCkeep$BGC)
+
+climlayer_filt<-subset(climlayer_filt, BGC %in% BGCkeep)
+
+#clean up and save
+climlayer_filt<- dplyr::select(climlayer_filt, -id, -exclude, -area2)
+save(climlayer_filt, file="trainingpts_w_clim_FILTERED.Rdata")
+training_points_filt<-dplyr::select(climlayer_filt, lon, lat, elev, BGC)%>%relocate(BGC, .before = lon) 
+write.csv(training_points_filt, "spatialdata/WNA_v13_50-200filtpts_15Nov.csv" )
 
 #spbal for balanced acceptance sampling----
 #NOT working 11/14/24
