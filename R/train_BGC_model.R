c9768fa0bc5abfe5433d08d4b8cd3ab3204842ad
Courtney Collins <Courtney.Collins@gov.bc.ca>
2024-11-18 13:18:05 -0800

    train BGC model

---
+++ R/train_BGC_model.R
@@ -0,0 +1,38 @@
+#License info ----
+#Copyright 2019 Province of British Columbia
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+#pull in training data set (lat/longs)
+trainpts<-read.csv("spatialdata/WNA_v13_50-200filtpts_15Nov.csv")
+
+##pull in climate data for training pts
+my_grid<-trainpts
+my_grid<-dplyr::select(my_grid, -BGC)
+colnames(my_grid) <- c("id", "lon", "lat", "elev") # rename column names to what climr expects
+
+#which variables do we want? 
+varsl = c("CMD_sm", "DD_0_sp", "DD5_sp", "Eref_sm", "Eref_sp", "EXT", 
+          "MWMT", "NFFD_sm", "NFFD_sp", "PAS", "PAS_sp", "SHM", "Tave_sm", 
+          "Tave_sp", "Tmax_sm", "Tmax_sp", "Tmin", "Tmin_at", "Tmin_sm", 
+          "Tmin_sp", "Tmin_wt", "CMI", "PPT_MJ", "PPT_JAS", "CMD.total")
+
+## climr call- This will return the observed 1961-1990 climates for the raster grid points.
+cache_clear()
+gc()
+
+#only use if need to re-run climr 
+climlayer <- downscale(
+  xyz = my_grid,  which_refmap = "refmap_climr",
+  obs_periods = "2001_2020", 
+  vars = varsl)
+
+#save(climlayer, file="trainingpts_w_clim_Final.Rdata")
+
+
