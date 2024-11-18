07f0577c0d49aa3b73d41aaf572e5772249da35c
Courtney Collins <Courtney.Collins@gov.bc.ca>
2024-11-18 15:17:22 -0800

    train and save BGC prediction models

--- R/train_BGC_model.R
+++ R/train_BGC_model.R
@@ -18,10 +18,9 @@ library(sf)
 library(raster)
 library(tidyverse)
 
-
 #pull in training data set (lat/longs)
 trainpts<-read.csv("spatialdata/WNA_v13_50-200filtpts_15Nov.csv")
-
+#only use if need to re-run climr---- 
 ##pull in climate data for training pts
 my_grid<-trainpts
 my_grid<-dplyr::select(my_grid, -BGC)
@@ -38,10 +37,10 @@ cache_clear()
 gc()
 
 #only use if need to re-run climr 
-climlayer <- downscale(
-  xyz = my_grid,  which_refmap = "refmap_climr",
-  obs_periods = "2001_2020", 
-  vars = varsl)
+#climlayer <- downscale(
+#  xyz = my_grid,  which_refmap = "refmap_climr",
+#  obs_periods = "2001_2020", 
+#  vars = varsl)
 
 addVars <- function(dat) {
   dat[, PPT_MJ := PPT_05 + PPT_06]
@@ -53,5 +52,50 @@ addVars <- function(dat) {
 }
 
 save(climlayer, file="trainingpts_w_clim_Final.Rdata")
+
+#fit mods----
+#load climate dataset
+load(file="trainingpts_w_clim_Final.Rdata")
+model_data<-climlayer
+rm(climlayer)
+model_data<-subset(model_data, PERIOD=="1961_1990")
+#merge with BGC info 
+trainpts<-rename(trainpts, id=X)
+model_data<-left_join(model_data, trainpts)
+model_data$BGC<-as.factor(model_data$BGC)
+model_data<-dplyr::select(model_data, -id, -lat, -lon, -elev, -PERIOD)
+model_data<-na.omit(model_data) #removes 47 obs with NAs in CMI
+#make sure still >50 obs for all BGCs
+BGSpts<-group_by(model_data, BGC)%>%summarise(ct=n())
+min(BGCpts$ct)
+
+# Fit RF models- response and probability  
+BGC_RFresp<- ranger::ranger(BGC~ .,data = model_data, mtry= 5, classification = T, probability = F, 
+                            num.trees = 501, splitrule =  "extratrees", min.node.size = 2,
+                            importance = "permutation",write.forest = TRUE) 
+save(BGC_RFresp, file="model_output/BGC_RFresp.Rdata")# CGC local only 
+
+#they are in here 
+#F:/OneDrive - Government of BC/WNA_BGC/Trained_Models/
+  
+BGC_RFprob<- ranger::ranger(BGC~ .,data = model_data, mtry= 5, classification = T, probability = F, 
+                             num.trees = 501, splitrule =  "extratrees", min.node.size = 2,
+                             importance = "permutation",write.forest = TRUE) 
+save(BGC_RFresp, file="model_output/BGC_RFprob.Rdata")# CGC local only 
+#they are in here 
+#F:/OneDrive - Government of BC/WNA_BGC/Trained_Models/
+
+
+#error estimates 
+#oob estimates 
+print(BGC_RFresp)#0.24
+print(BGC_RFprob) #0.24
+
+#confusion matrices on fitted data 
+cf<-BGC_RFresp$confusion.matrix
+accuracy <- sum(diag(cf)) / sum(cf) #acc = 0.76 
+
+cf2<-BGC_RFprob$confusion.matrix
+accuracy <- sum(diag(cf2)) / sum(cf2) #acc = 0.76 
 
 
