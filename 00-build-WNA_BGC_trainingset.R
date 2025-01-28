955c6ec0e5a9b3d2716b453da6b83d87c0375349
Obrist <Debora.Obrist@gov.bc.ca>
2025-01-27 16:07:12 -0800

    Cleaning up the code post suggestions from Kiri and Will

--- 00-build-WNA_BGC_trainingset.R
+++ 00-build-WNA_BGC_trainingset.R
@@ -25,22 +25,21 @@ options(reproducible.cachePath = "reproducible.cache/",
 
 #### Create training points: ####
 # Load in BGC polygons: 
-# QUESTION 1: What version of the BGC data should I use? 
-bgcs <- st_read("C:/Users/dobrist/Government of BC/Future Forest Ecosystems Centre - CCISS - CCISS/BGCProjections/Proxy_ObjectStorage/CCISS_Working/WNA_BGC/WNA_BGC_v12_5Apr2022/WNA_BGC_v12_5Apr2022.gpkg")
+# TO DO: Update this with v13 once available. 
+bgcs <- st_read("C:/Users/dobrist/Government of BC/Future Forest Ecosystems Centre - CCISS - CCISS/ccissv13_workingfiles/BGC_modelling/WNA_BGC_v13_15Nov2024.gpkg")
 # bgcs <- st_read("//objectstore2.nrs.bcgov/ffec/CCISS_Working/WNA_BGC/WNA_BGC_v12_5Apr2022/WNA_BGC_v12_5Apr2022.gpkg")
 
-
+# TO DO: Find 30 m DEM. (Will says best to use 30 m DEM because climr vars depend on elevation). 
 # And the DEM:
-# QUESTION 2: Which DEM should I use?  
-elev <- rast("C:/Users/dobrist/Government of BC/Future Forest Ecosystems Centre - CCISS - CCISS/BGCProjections/Proxy_ObjectStorage/DEM/DEM_Composite_WNA_800m/composite_WNA_dem.tif")
+elev <- rast("C:/Users/dobrist/Government of BC/Future Forest Ecosystems Centre - CCISS - CCISS/ccissv13_workingfiles/BGC_modelling/WNA_DEM_4326_clipped.tif")
 # elev <- rast("//objectstore2.nrs.bcgov/ffec/DEM/DEM_Composite_WNA_800m/composite_WNA_dem.tif")
 
 # Reproject elev to Albers: 
 elev <- project(elev, crs(bgcs))
 
-# Define smaller study area for script building purposes. These are the extents in lat/long but we want them in Albers instead.
+# Define smaller test study area. These are the extents in lat/long but we want them in Albers instead.
 
-# Remove this later and run script on entire training area instead. 
+# TO DO: Remove this later and run script on entire training area instead. 
 # trainingarea <- ext(c(-125, -112, 43, 55))
 studyarea <- ext(c(-123, -117, 49, 52.5))
 
@@ -57,17 +56,16 @@ studyarea_albers <- ext(dummy_raster_albers)
 elev <- crop(elev, studyarea_albers)
 bgcs <- st_crop(bgcs, studyarea_albers)
 
+# TO DO: Find out if this is necessary. If so, what is a good land-only polygon to use? If not, what is the justification? (e.g., Colin mentioned that we want some overlap with ocean to make sure we hit islands etc)
+
 # From example in climr documentation: "Since climr is meant to be used to downscale climate variables in land, we will “clip” (set values outside the polygon to NAs) the raster using a land-only polygonRemove areas with water: 
 
-# QUESTION 3: Do I need to do this? If so, what is a good land-only polygon to use? If not, what is the justification? (e.g., Colin mentioned that we want some overlap with ocean to make sure we hit islands etc)
 # elev <- mask(elev, bgcs)
 
+
+# TO DO: Decide/test whether it makes a difference if we randomly sample a set number of points (if so, how many) per BGC, or if we do a balanced sampling approach. If balanced, what kind of approach? 
+
 # This function makes a grid over the extent of bgcs, and fills it with a dummy variable (1L), converts to a spatial vector. It extracts elevation at the grid points using bilinear interpolation. 
-
-# QUESTION 4: Gridsize = 2 km. Are we still happy with this? Justification from RMD is:  
-# "A 2km grid seems to provide enough training points for most BGCs. Large non-vegetation land areas are excluded (lakes and glaciers primarily)."
-
-# QUESTION 5: Do we want to do this? Or would it be simpler to just extract grid point every 800 m from DEM?
 coords <- makePointCoords(bgcs, elev, gridSize = 2000) |>
   Cache()
 
@@ -80,40 +78,31 @@ points_sf <- st_as_sf(coords, coords = c("x", "y"), crs = 3005)
 bgc_att <- st_join(points_sf, bgcs)
 bgc_att <- data.table(st_drop_geometry(bgc_att))
 
-# bgc_att has 48108 unique IDs but 48112 rows. 
+# bgc_att has 48108 unique IDs but 48109 rows. 
 length(unique(bgc_att$id))
 nrow(bgc_att)
-bgc_att[duplicated(bgc_att$id), ] # ids 4979, 4980, 10485, and 46026 are duplicated. 
+bgc_att[duplicated(bgc_att$id), ] # ids 4979 is duplicated in v13
 
-bgc_duplicates <- bgc_att[bgc_att$id %in% c(4979, 4980, 10485, 46026), ]
+bgc_duplicates <- bgc_att[bgc_att$id %in% c(4979), ]
 bgc_duplicates <- merge(bgc_duplicates, coords, by = c("id", "elev"))
+bgc_duplicates_sf <- st_as_sf(bgc_duplicates, coords = c("x", "y"), crs = 3005)
 
-# QUESTION 6: Figure out what's going on with these duplicates and what to do with them. 
-# Check what's going on with these duplicates: 
-checkarea <- ext(c(1651994 - 1000, 1651994 + 1000, 779989.6 - 10000, 779989.6 + 10000)) # xmin, xmax, ymin, ymax
+# TO DO: Figure out what's going on with these duplicates and what to do about them. 
+checkarea <- ext(c(1583996 - 2500, 1583996 + 2500, 827812.8 - 2500, 827812.8 + 2500)) # xmin, xmax, ymin, ymax
 bgcs_check <- st_crop(bgcs, checkarea)
-plot(bgcs_check, xlim = c(min(checkarea[1]), max(checkarea[2])), 
-     ylim = c(min(checkarea[3]), max(checkarea[4]))) 
-points(x = bgc_duplicates$x[c(5, 6)], y = bgc_duplicates$y[c(5, 6)], col = "black", pch = 16)
+plot(st_geometry(bgcs_check), col = as.factor(bgcs_check$BGC))
+plot(st_geometry(bgc_duplicates_sf), col = "black", pch = 16, add = TRUE)
+text(st_coordinates(bgc_duplicates_sf), labels = bgc_duplicates_sf$id, cex = 0.7, pos = 3, col = "black")
 
-# Remove duplicates for now: 
+# Remove this duplicate for now: 
 bgc_att <- unique(bgc_att, by = "id")
 
 # Also remove rows where BGC is NA: 
+nrow(bgc_att[is.na(bgc_att$BGC)]) # 70
 bgc_att <- bgc_att[!is.na(bgc_att$BGC), ]
 
-# Summarize how many points in each  BGC.   
-# Need to check what the numbers are like when using the full extent. 
-# NOTE - this step might not be necessary as there is a utils function to do this later on. 
-
-# QUESTION 7: Does it make more sense to do this here and now or later, after we have climr vars? 
-
-BGC_counts <- bgc_att[, .(Num = .N), by = .(BGC)] 
-# BGC_counts <- BGC_counts[Num >= 10] # For now, retain only those where N > 10. 
-bgc_att_filtered <- bgc_att[BGC %in% BGC_counts$BGC]
-
 # Merge coords and BGC data from bgc_att: 
-coords2 <- merge(coords, bgc_att_filtered, by = c("id", "elev"))
+coords <- merge(coords, bgc_att, by = c("id", "elev"))
 
 # Crops the coordinates to the study area defined above: 
 # Note: coords_train will be identical to coords for now since I already cropped to the size of the smaller study area earlier but when I rerun with entire training area, it will be different. 
@@ -121,7 +110,7 @@ coords_train <- subsetByExtent(coords2, studyarea_albers)
 
 # Make rectangular gap extents within the bounding box of the study area. 5L is the default number of gaps to create. 
 
-# QUESTION 8: Is the idea that I will eventually make these gaps over the entire training area (i.e., BC, US, AB) to train a "final" model? 
+# QUESTION 8: Is the idea that I will eventually make these gaps over the entire training area (i.e., BC, US, AB) to train and check a "final" model? 
 gapextents <- makeGapExtents(studyarea_albers, 5L)
 
 # Convert list of spatial extents into to polygons: 
@@ -148,8 +137,8 @@ points(x = coords_gaps$x, y = coords_gaps$y, col = "black", cex = 0.001) # Just 
 points(x = coords_trainWgaps$x, y = coords_trainWgaps$y, col = "white", cex = 0.001) # Everything but the gaps.
 
 # Recombine coords_gaps and coords_trainingWgaps but with an extra column for "gap = 0 or 1" for holdouts. 
-coords_gaps[, gap := "0"]
-coords_trainWgaps[, gap:= "1"]
+coords_gaps[, gap := "yes"]
+coords_trainWgaps[, gap:= "no"]
 
 coords_all <- rbind(coords_gaps, coords_trainWgaps)
 
@@ -172,11 +161,16 @@ coords_all <- coords_all %>%
   select(id, lon, lat, elev, BGC, gap, x, y)
 
 # Define variables needed: 
+list_vars()
 
-# QUESTION 9: Does it make sense to pull all variables we might want at once, and subset for model later? If so, what are all the relevant variables? 
+# First, just seasonal PPT, Tmax, Tmin: 
+vars_simple <- c("PPT_sp", "PPT_sm", "PPT_at", "PPT_wt", 
+                 "Tmax_sp", "Tmax_sm", "Tmax_at", "Tmax_wt", 
+                 "Tmin_sp", "Tmin_sm", "Tmin_at", "Tmin_wt")
 
-# First, just simply PPT, Tmax, Tmin: 
-vars_simple <- c("PPT", "Tmax", "Tmin")
+
+
+
 
 # QUESTION 10: These are the variables previously selected. Colin and Kiri mentioned the possibility of testing three subsets of climate variables: seasonal or monthly PPT/Tmax/Tmin, a pairwise selection, and an "ecologically relevant" subset. Is this the ecologically relevant subset? If not, what should be included in that instead? (Note: check AddVars() function to add more.)
 # Define a more complex set: 
@@ -185,13 +179,11 @@ vars_more <- c("DD5", "DD_0_at", "DD_0_wt", "PPT05", "PPT06", "PPT07", "PPT08",
 
 # QUESTION 10: Is there existing code for the "pairwise" variable selection? 01-23-25 - https://github.com/bcgov/Build_WNA_BGC_model/blob/development/R/LocalFeatureSelection.R. 
 
-# QUESTION 11: Lots of questions with this prompt - obs_periods, gcm_periods, gcms, ssps, max_run? 01-23-25 - Discussed with Kiri - This is all that is required for now. 
-
-# Pull data from climr: 
+# Pull data from climr:
 clim_vars <- downscale(
   xyz = coords_all,
-  which_refmap = "refmap_climr", 
-  return_refperiod = TRUE, # Also return the 1961-1990 normals period. 
+  which_refmap = "refmap_climr",
+  return_refperiod = TRUE, # Also return the 1961-1990 normals period.
   vars = vars_simple,
   cache = TRUE)|>
   Cache()
@@ -208,9 +200,6 @@ coords_all  <- coords_all %>%
 # First, add long, lat, elevation, BGC, x, y, and gap back in: 
 trainData <- left_join(clim_vars, coords_all, relationship = "many-to-many") %>%
   distinct()
-
-# For now, just look at the reference period: 
-trainData <- trainData[PERIOD == "1961_1990"]
 
 # QUESTION 13: Which BGCs should be included or not? Should they be removed based on number of points per BGC, or some metric from the climr data (e.g., CV for parameters within BGCs?) 
 
@@ -248,26 +237,24 @@ trainData_balanced[, BGC := as.factor(BGC)]
 
 cols <- c("BGC", vars_simple)
 
-# QUESTION 16: How to decide on parameters here? Also need to figure out why it works when I manually separate holdout(gap) vs non-gap data but when I run within the function it crashes R. 
+# Run the RF model with the entire area: 
 BGCmodel_full <- ranger(
   BGC ~ .,
   data = trainData_balanced[, ..cols],
   num.trees = 501,
   splitrule =  "extratrees",
-  mtry = 2,
+  # mtry = 2, # Allow for default.
   min.node.size = 2,
   importance = "permutation",
- # case.weights = trainData_balanced$gap,
- # holdout = TRUE,
   write.forest = TRUE,
   classification = TRUE,
   probability = FALSE,
   
 ) |>
   Cache()
-
 beepr::beep()
 
+# Run the model on area outside of the gaps: 
 trainData_balanced_Wgaps <- trainData_balanced[gap == 1]
 
 BGCmodel_Wgaps <- ranger(
@@ -275,15 +262,12 @@ BGCmodel_Wgaps <- ranger(
   data = trainData_balanced_Wgaps[, ..cols],
   num.trees = 501,
   splitrule =  "extratrees",
-  mtry = 2,
+ # mtry = 2, # Allow for default.
   min.node.size = 2,
   importance = "permutation",
-  # case.weights = trainData_balanced$gap,
-  # holdout = TRUE,
   write.forest = TRUE,
   classification = TRUE,
   probability = FALSE,
-  
 ) |>
   Cache()
 beepr::beep()
@@ -291,20 +275,58 @@ beepr::beep()
 # QUESTION 17: How do we diagnose the models? What are we "happy" with? Also, what output specifically is required for CCISS? Accuracy/Precision/Recall/F1 score/AUC PR/ROC AUC/etc. 
 # Check the models: 
 conf_matrix_full <- caret::confusionMatrix(data = predictions(BGCmodel_full),
-                reference = trainData_balanced$BGC)
-
+                                           reference = trainData_balanced$BGC)
 conf_matrix_Wgaps <- caret::confusionMatrix(data = predictions(BGCmodel_Wgaps),
                        reference = trainData_balanced_Wgaps$BGC)
 
-# Metrics (have not run)
-precision <- conf_matrix_full$byClass["Precision"]
-recall <- conf_matrix_full$byClass["Recall"]
-f1 <- conf_matrix_full$byClass["F1"]
-balanced_accuracy <- conf_matrix_full$byClass["Balanced Accuracy"]
+print(BGCmodel_full) # OOB prediction error: 23.37%
+print(BGCmodel_Wgaps) # OOB prediction error: 23.13%
 
-print(BGCmodel_full) # OOB prediction error: 52.91%
-print(BGCmodel_Wgaps) # OOB prediction error: 50.39%
+# Generate predictions based on the coordinates in the gaps: 
+# Make testing data with gap data (gap = 0): 
+trainData_balanced_gaps <- trainData_balanced[gap == 0]
 
-# Save predictions (also need these for leaflet script): 
-predictions_full <- predict(BGCmodel_full, data = trainData_balanced)$predictions
-predictions_Wgaps <- predict(BGCmodel_Wgaps, data = trainData_balanced_Wgaps)$predictions
+# Predictions based on full model and gap removed model: 
+predictions_full <- predict(BGCmodel_full, data = trainData_balanced_gaps)
+predictions_Wgaps <- predict(BGCmodel_Wgaps, data = trainData_balanced_gaps)
+
+# Save into trainData_balanced_gaps testing dataframe:
+trainData_balanced_gaps$predictions_full <- as.character(predictions_full$predictions)
+trainData_balanced_gaps$predictions_Wgaps <- as.character(predictions_Wgaps$predictions)
+
+# Check how well each model performed at predicting gaps:  
+conf_matrix_testgaps_full <- caret::confusionMatrix(
+  data = factor(trainData_balanced_gaps$predictions_full, levels = levels(trainData_balanced_gaps$BGC)),
+  reference = factor(trainData_balanced_gaps$BGC, levels = levels(trainData_balanced_gaps$BGC))
+)
+
+conf_matrix_testgaps_Wgaps <- caret::confusionMatrix(
+  data = factor(trainData_balanced_gaps$predictions_Wgaps, levels = levels(trainData_balanced_gaps$BGC)),
+  reference = factor(trainData_balanced_gaps$BGC, levels = levels(trainData_balanced_gaps$BGC))
+)
+
+# Print accuracy of each on new data: 
+accuracy_full_on_gaps <- mean(trainData_balanced_gaps$predictions_full == trainData_balanced_gaps$BGC)
+print(paste("Accuracy on new data:", accuracy_full_on_gaps))
+
+accuracy_Wgaps_on_gaps <- mean(trainData_balanced_gaps$predictions_Wgaps == trainData_balanced_gaps$BGC)
+print(paste("Accuracy on new data:", accuracy_Wgaps_on_gaps))
+
+# Look at feature importance: 
+importance_scores_Wgaps <- BGCmodel_Wgaps$variable.importance
+importance_scores_Wgaps <- sort(importance_scores_Wgaps, decreasing = TRUE)
+
+# Create a data frame for plotting
+importance_df <- data.frame(
+  Feature = names(importance_scores_Wgaps),
+  Importance = importance_scores_Wgaps
+)
+
+# Plot
+ggplot(importance_df, aes(x = reorder(Feature, Importance), y = Importance)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  coord_flip() +
+  labs(title = "Feature Importance",
+       x = "Features",
+       y = "Importance") +
+  theme_minimal()
---
+++ LocalFeatureSelection_DSO.R
@@ -0,0 +1,148 @@
+library(data.table)
+library(ranger)
+library(foreach)
+library(terra)
+library(sf)
+library(dplyr)
+library(climr)
+library(vip)
+library(tictoc)
+
+addVars <- function(dat){ ##this function modifies everything inplace, so no need for return value
+  dat[,`:=`(PPT_MJ = PPT05+PPT06,
+            PPT_JAS = PPT07+PPT08+PPT09,
+            PPT.dormant = PPT_at+PPT_wt)]
+  dat[,`:=`(CMD.def = 500-PPT.dormant)]
+  dat[CMD.def < 0, CMD.def := 0]
+  dat[,`:=`(CMDMax = CMD07,
+            CMD.total = CMD.def + CMD)]
+  dat[,`:=`(CMD.grow = CMD05+CMD06+CMD07+CMD08+CMD09,
+            DD5.grow = DD5_05+DD5_06+DD5_07+DD5_08+DD5_09,
+            #DDgood = DD5 - DD18,
+            #DDnew = (DD5_05+DD5_06+DD5_07+DD5_08)-(DD18_05+DD18_06+DD18_07+DD18_08),
+            TmaxJuly = Tmax07)]
+}
+
+bgc_map <- st_read("D:/OneDrive - Government of BC/WNA_BGC - Future Forest Ecosystems Centre/ccissv13_files/WNA_BGC_v13_15Nov2024.gpkg") %>% dplyr::filter(!BGC == "(None)")
+bgc_map$ID <- seq_along(bgc_map$BGC)
+bgcs <- unique(bgc_map$BGC) %>% data.frame
+fwrite(bgcs, "wna_bgcs.csv")
+
+bgc_info <- fread("D:/OneDrive - Government of BC/WNA_BGC - Future Forest Ecosystems Centre/ccissv13_files/WNA_BGCs_Info_v13_1.csv") %>% filter(BGC %in% bgcs$.) #%>% mutate(bgc = as.factor(BGC))
+BC_BGCs <- bgc_info[grep("BGC.*",Source),BGC]
+
+#bgc_map <- st_read("BC_BGCs_with_ID.gpkg")
+
+neighbours_ls <- list()
+#for(bgc in BC_BGCs[-(1:180)]){
+for(bgc in bgc_info$BGC){
+  cat(".")
+  focal <- bgc_map[bgc_map$BGC == bgc,]
+  if(nrow(focal) > 0){
+    focal <- st_union(focal$geom)
+    neighbours <- st_intersects(focal, bgc_map)
+    neighbours_ls[[bgc]] <- neighbours[[1]]
+  }
+}
+
+saveRDS(neighbours_ls,"WNA_BGC_NeighbourList.rds",)
+
+st_write(bgc_map, "WNA_BGCs_with_ID.gpkg", append = FALSE)
+
+#dem <- terra::rast("D:/CommonTables/DEMs/WNA_DEM_4326_clipped.tif")
+saveRDS(dem, "//objectstore2.nrs.bcgov/ffec/CCISS_Working/WNA_DEM_4326_clippedDEM.rds")
+# %>% terra::project("epsg:4326")
+# writeRaster(dem2, "D:/CommonTables/DEMs/WNA_DEM_4326_clipped.tif")
+#dem <- rast("//objectstore2.nrs.bcgov/ffec/DEM/DEM_NorAm/NA_Elevation/data/northamerica/northamerica_elevation_cec_2023.tif")
+
+# dem <- terra::rast("D:/CommonTables/DEMs/WNA_DEM_SRT_30m_cropped.tif")
+# dem2 <- crop(dem, ext(st_transform(bgc_map, 4326)))
+#dem.noram <- rast("//objectstore2.nrs.bcgov/ffec/DEM/DEM_NorAm/NA_Elevation/data/northamerica/northamerica_elevation_cec_2023.tif")
+#dem <- dem.noram
+
+neighbours_ls <- readRDS("WNA_BGC_NeighbourList.rds")
+remove = c("PPT_at", "PPT_wt", "CMD.def")
+vars.selected <- fread("no_month_vars.csv") %>% dplyr::filter(!vars %in% remove)
+
+bgc_list <- names(neighbours_ls)
+bgc_list <- bgc_list[!grepl("BAFA.* | *.un.", bgc_list)]
+bgc_list <- bgc_list[-1]
+
+tic()
+
+vars <- c(vars.selected$vars,"BGC")
+bgc_list_short <- c("BGxh3", "BWBSdk", "CDFmm", "ICHmc1", "CWHmm1", "ESSFwk1","ICHmw1","IDFdk3",
+                    "IDFxx2","MSdw","SBSdk","MSxv")
+bgc_list <- bgc_list[-"CWHvm2"]
+tic()
+res_list <- list()
+bgc = "CDFmm"
+for(bgc in bgc_list){
+  cat("Processing",bgc,"\n")
+  out <- bgc_map[bgc_map$ID %in% neighbours_ls[[bgc]],]
+  out_union <- group_by(out, BGC) %>% 
+    summarize(geom = st_union(geom),
+              BGC = BGC[1])
+  pnts <- st_sample(out_union, size = rep(150, nrow(out_union)), type = "random", by_polygon = T)
+  pnts_all <- st_as_sf(data.frame(BGC = rep(out_union$BGC, each = 150), geometry = pnts))
+  pnts_all <- st_transform(pnts_all, 4326)
+  coords <- st_coordinates(pnts_all)
+  temp_elev <- terra::extract(dem, coords)
+  coords.bgc <- data.frame(coords, elev = temp_elev$WNA_DEM_SRT_30m,
+                           ID = 1:nrow(pnts_all), BGC = pnts_all$BGC) %>% rename(lat = Y, lon = X, id = ID) %>% 
+    dplyr::select(lon, lat, elev,id, BGC)
+  coords <- coords.bgc %>%   dplyr::select(lon, lat, elev,id)
+  coords.bgc <- coords.bgc %>% dplyr::select(id, BGC)
+  # coords <- data.frame(coords, elev = temp_elev$WNA_DEM_SRT_30m, 
+  #                      ID = 1:nrow(pnts_all), BGC = pnts_all$BGC)
+  
+  clim_vars <- suppressMessages(climr_downscale(coords, which_normal = "auto", 
+                                                vars = list_variables(), return_normal = T))  
+  clim_vars <- data.table:::na.omit.data.table(clim_vars)
+  addVars(clim_vars)
+  clim_vars <- left_join(clim_vars, coords.bgc)
+  
+  clim_vars <- setDT(clim_vars)[,..vars]
+  
+  clim_vars[,BGC := as.factor(BGC)]
+  rf_mod <- ranger(BGC ~ ., data = clim_vars, num.trees = 101, importance = "impurity", splitrule = "gini")
+  varimp <- sort(importance(rf_mod),decreasing = T)[1:6]
+  res_list[[bgc]] <- data.table(Focal = bgc, Var = names(varimp), 
+                                Importance = unname(varimp), 
+                                OOB = rf_mod$prediction.error,
+                                NumberBGCs = nrow(out_union))
+}
+
+
+dat_all2 <- rbindlist(res_list)
+fwrite(dat_all2, "Focal_Variable_Importance_WNAv1.csv")
+var.count <- dat_all2[,.(Num = .N), by = .(Var)]
+setorder(test, -Num)
+toc()
+fwrite(var.count, "Count_of_Variables_v2.csv")
+#### Add type of variable by 3 categories and look by BGC
+### GS/NGS, Temp?Precip?interact, Extreme/Mean
+
+mean.error <- dat_all2 %>% select(Focal, OOB ) %>% distinct %>% mutate(mean.error = mean(OOB))
+mean.error2 <- mean.error %>% filter(!str_detect(Focal, "p$"))%>% filter(!str_detect(Focal, "w$"))
+require(ggplot2);require(stringr)
+
+
+ggplot(mean.error2, aes(x="Y" , y=OOB))+
+  geom_violin()+
+  stat_summary(fun.data = "mean_sdl",  geom="crossbar", width=0.05)
+
+var.type <- fread("Variable_Types.csv")
+dat_all2 <- fread("Focal_Variable_Importance_WNAv1.csv") %>% left_join(var.type)
+BGC_using_group <- dat_all2 %>% group_by(Focal, var.group, var.season) %>% count() %>% select(-n) %>%  ungroup() %>% group_by(var.group, var.season) %>% count()
+var.count <- dat_all2[,.(Num = .N), by = .(var.type)]
+setorder(test, -Num)
+
+# 
+# st_write(out, "Test_IDFdk3_Neighbours.gpkg")
+# st_write(pnts_all, "Test_pnts.gpkg")
+# 
+# bgc_vect <- vect(bgc_map)
+# focal_vect <- vect(focal)
+# test <- terra::nearby(focal_vect,bgc_vect,distance = 0.1)
+# plot(bgc_vect[test[,2]])
