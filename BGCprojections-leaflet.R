8844801bfa59b5732ac21fe66021ee98f1036ec6
Obrist <Debora.Obrist@gov.bc.ca>
2025-01-29 16:41:53 -0800

    Now works for colours - needs to be updated to run stand alone and with different models.

--- BGCprojections-leaflet.R
+++ BGCprojections-leaflet.R
@@ -1,53 +1,32 @@
 library(leaflet)
 
-# Combine data into a single data frame for plotting
-trainData_balanced_TEST <- trainData_balanced
+# # Add in the predictions for the model version with no gaps/holdouts. 
+# trainData_balanced_gaps$predictions_full <- predictions_full$predictions
+# 
+# # Deal with gaps/holdouts: 
+# trainData_balanced_gaps$predictions_Wgaps <- NA
+# trainData_balanced_gaps$predictions_Wgaps[trainData_balanced_gaps$gap == "yes"] <- as.character(predictions_Wgaps$predictions)
+trainData_balanced_gaps2 <- trainData_balanced_gaps
 
-# Add in the predictions for the model version with no gaps/holdouts. 
-trainData_balanced_TEST$predictions_full <- predictions_full
-
-# Deal with gaps/holdouts: 
-trainData_balanced_TEST$predictions_Wgaps <- NA
-trainData_balanced_TEST$predictions_Wgaps[trainData_balanced_TEST$gap == 1] <- as.character(predictions_Wgaps)
-
-mismatched_predictions <- trainData_balanced_TEST[predictions_Wgaps != BGC]
-
-
-# Define color palettes for the models
+# Define color palettes for trainData_balanced_gaps2# Define color palettes for the models
 subzones_colours_ref <- fread("C:/Users/dobrist/Government of BC/Future Forest Ecosystems Centre - CCISS - CCISS/CCISSv13_latest_tool_materials/WNAv13_Subzone_colours_2.csv") %>% 
   dplyr::select(!c(fid, MAP_LABEL, NSRNAME, ZONE)) 
 
 # Match up colors and zones: 
-trainData_balanced_TEST <- merge(
-  trainData_balanced_TEST,
-  subzones_colours_ref,  
-  by.x = "predictions_full",  
-  by.y = "BGC",
-  all.x = TRUE
-)
+trainData_balanced_gaps[, predictions_full := as.character(predictions_full)]
+subzones_colours_ref[, BGC := as.character(BGC)]
 
-# Rename the color column for clarity
-setnames(trainData_balanced_TEST, "RGB", "color_full")
+trainData_balanced_gaps <- trainData_balanced_gaps[subzones_colours_ref, 
+                                                   on = .(predictions_full = BGC), 
+                                                   nomatch = 0][
+                                                     , color_full := RGB
+                                                   ][, RGB := NULL]
 
-# Add colors for predictions_Wgaps
-trainData_balanced_TEST <- merge(
-  trainData_balanced_TEST,
-  subzones_colours_ref,
-  by.x = "predictions_Wgaps", # Match predictions_Wgaps with the BGC in bgc_colors
-  by.y = "BGC",
-  all.x = TRUE
-)
-
-# Rename the second color column
-setnames(trainData_balanced_TEST, "RGB", "color_Wgaps")
-
-
-# Some colors missing: 
-setdiff(trainData_balanced_TEST$BGC, trainData_balanced_TEST2$BGC) # "CWHdm"  "CWHxm1" "IMAunp"
-nrow(trainData_balanced_TEST[trainData_balanced$BGC %in% c("CWHdm", "CWHxm1", "IMAunp")])
-
-
-# There is not a colour assigned to "CWHdm"  "CWHxm1" "IMAunp". Ignore for now. 
+trainData_balanced_gaps <- trainData_balanced_gaps[subzones_colours_ref, 
+                                                   on = .(predictions_Wgaps = BGC), 
+                                                   nomatch = 0][
+                                                     , color_Wgaps := RGB
+                                                   ][, RGB := NULL]
 
 # Also merge with bgcs data for comparison. Reproject to lat/long as required by leaflet: 
 bgcs2 <- merge(bgcs, subzones_colours_ref, by = "BGC")
@@ -58,7 +37,7 @@ bgcs2 <- st_transform(bgcs2, crs = 4326)
 gap_poly2 <- project(gap_poly, "EPSG:4326")
 
 # Leaflet:
-leaflet(trainData_balanced_TEST) %>%
+leaflet(trainData_balanced_gaps) %>%
   addTiles() %>%
   addPolygons(
     data = bgcs2,            
@@ -103,5 +82,10 @@ leaflet(trainData_balanced_TEST) %>%
 
 beepr::beep()
 
-mismatched_predictions <- trainData_balanced_TEST[predictions_full != predictions_Wgaps]
-mismatched_predictions <- trainData_balanced_TEST[predictions_full != predictions_Wgaps]
+mismatched_predictions <- trainData_balanced_gaps[predictions_full != BGC]
+mismatched_predictions2 <- trainData_balanced_gaps[predictions_Wgaps != BGC]
+mismatched_predictions3 <- trainData_balanced_gaps[predictions_Wgaps != predictions_full]
+
+nrow(mismatched_predictions)
+nrow(mismatched_predictions2)
+nrow(mismatched_predictions3)
