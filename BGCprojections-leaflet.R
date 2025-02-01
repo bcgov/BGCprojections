ab1955ae4185e0f9b755d2c874f23e4572568836
Obrist <Debora.Obrist@gov.bc.ca>
2025-01-31 16:26:20 -0800

    updates

--- BGCprojections-leaflet.R
+++ BGCprojections-leaflet.R
@@ -4,55 +4,11 @@ library(leaflet)
 subzones_colours_ref <- fread("C:/Users/dobrist/Government of BC/Future Forest Ecosystems Centre - CCISS - CCISS/CCISSv13_latest_tool_materials/WNAv13_Subzone_colours_2.csv") %>% 
   dplyr::select(!c(fid, MAP_LABEL, NSRNAME, ZONE)) 
 
-# Create template raster: 
-gap_DEM <- crop(elev, ext(gap_poly))
-gap_DEM <- mask(gap_DEM, gap_poly)
-plot(gap_DEM)
-
-# Convert predictions_full to a factor
-trainData_balanced_gaps[, predictions_full := as.factor(predictions_full)]
+# Reproject elev raster to WGS84 (EPSG:4326)
+elev <- project(elev, "EPSG:4326")
 
 # Assign predictions to raster cells
-gap_DEM[trainData_balanced_gaps[, id]] <- trainData_balanced_gaps[, predictions_full]
-
-# Plot the resulting raster
-plot(gap_DEM, main = "Predictions Full")
-
-
-
-# Convert trainData_balanced_gaps coordinates into a spatial object
-train_coords <- trainData_balanced_gaps[, .(lon, lat)]  # Extract lon/lat
-train_sp <- vect(train_coords, crs = crs(gap_DEM))  # Make it a spatial object with the same CRS as the raster
-
-# Get the corresponding cell indices in the raster based on the coordinates
-cell_indices <- cellFromXY(gap_DEM, train_sp)
-
-# Assign the values to the raster based on those indices
-values(gap_DEM)[cell_indices] <- as.character(trainData_balanced_gaps$predictions_full)
-
-# Check the result
-summary(values(gap_DEM))
-
-
-
-
-
-
-# Match up colors and zones: 
-trainData_balanced_gaps[, predictions_full := as.character(predictions_full)]
-subzones_colours_ref[, BGC := as.character(BGC)]
-
-trainData_balanced_gaps <- trainData_balanced_gaps[subzones_colours_ref, 
-                                                   on = .(predictions_full = BGC), 
-                                                   nomatch = 0][
-                                                     , color_full := RGB
-                                                   ][, RGB := NULL]
-
-trainData_balanced_gaps <- trainData_balanced_gaps[subzones_colours_ref, 
-                                                   on = .(predictions_Wgaps = BGC), 
-                                                   nomatch = 0][
-                                                     , color_Wgaps := RGB
-                                                   ][, RGB := NULL]
+elev[clim_vars_preds[, id]] <- clim_vars_preds[, preds_full_simple]
 
 # Also merge with bgcs data for comparison. Reproject to lat/long as required by leaflet: 
 bgcs2 <- merge(bgcs, subzones_colours_ref, by = "BGC")
@@ -62,8 +18,15 @@ bgcs2 <- st_transform(bgcs2, crs = 4326)
 # First, reproject to lat/long: 
 gap_poly2 <- project(gap_poly, "EPSG:4326")
 
+# Create a color factor mapping BGC to RGB colors
+color_pal <- colorFactor(
+  palette = subzones_colours_ref$RGB,  # The RGB values from your table
+  domain = subzones_colours_ref$BGC   # The BGC categories from your table
+)
+
+
 # Leaflet:
-leaflet(trainData_balanced_gaps) %>%
+leaflet(clim_vars_preds) %>%
   addTiles() %>%
   addPolygons(
     data = bgcs2,            
@@ -85,24 +48,14 @@ leaflet(trainData_balanced_gaps) %>%
     popup = ~paste("Gap Polygon"),  
     group = "Gap Extents"
   ) %>% 
-  addCircleMarkers(
-    lat = ~lat, lng = ~lon,  
-    color = ~color_full,           
-    radius = 5, 
-    fillOpacity = 0.7,
-    popup = ~paste("Prediction:", predictions_full),
-    group = "Predictions - All"  
+  addRasterImage(
+    elev, # This is the reprojected raster
+    colors = color_pal,  # Apply the color palette
+    opacity = 0.5,     # Set the opacity of the raster layer
+    group = "Preds: full, simple"
   ) %>%
-  addCircleMarkers(
-    lat = ~lat, lng = ~lon,  
-    color = ~color_Wgaps,            
-    radius = 5,
-    fillOpacity = 0.7,
-    popup = ~paste("Prediction with Gaps:", predictions_Wgaps),
-    group = "Predictions with Gaps"  
-  ) %>% 
   addLayersControl(
-    overlayGroups = c("Predictions - All", "Predictions with Gaps", "BGC Zones", "Gap Extents"),  
+    overlayGroups = c("BGC Zones", "Gap Extents", "Preds: full, simple"),  
     options = layersControlOptions(collapsed = FALSE)  
   )
 
@@ -115,3 +68,45 @@ mismatched_predictions3 <- trainData_balanced_gaps[predictions_Wgaps != predicti
 nrow(mismatched_predictions)
 nrow(mismatched_predictions2)
 nrow(mismatched_predictions3)
+
+# # Match up colors and zones: 
+# trainData_balanced_gaps[, predictions_full := as.character(predictions_full)]
+# subzones_colours_ref[, BGC := as.character(BGC)]
+# 
+# trainData_balanced_gaps <- trainData_balanced_gaps[subzones_colours_ref, 
+#                                                    on = .(predictions_full = BGC), 
+#                                                    nomatch = 0][
+#                                                      , color_full := RGB
+#                                                    ][, RGB := NULL]
+# 
+# trainData_balanced_gaps <- trainData_balanced_gaps[subzones_colours_ref, 
+#                                                    on = .(predictions_Wgaps = BGC), 
+#                                                    nomatch = 0][
+#                                                      , color_Wgaps := RGB
+#                                                    ][, RGB := NULL]
+
+# Ensure preds_full_simple is a factor with correct levels
+clim_vars_preds$preds_full_simple <- factor(clim_vars_preds$preds_full_simple, levels = subzones_colours_ref$BGC)
+
+leaflet() %>%
+  addTiles() %>%
+  addPolygons(
+    data = bgcs2,            
+    fillColor = ~color_pal(BGC),
+    color = ~color_pal(BGC),
+    weight = 1,              
+    opacity = 1,             
+    fillOpacity = 0.5,       
+    popup = ~paste("Zone:", BGC),
+    group = "BGC Zones"
+  ) %>%
+  addRasterImage(
+    elev,  
+    colors = color_pal,  
+    opacity = 0.5,     
+    group = "Preds: full, simple"
+  ) %>%
+  addLayersControl(
+    overlayGroups = c("BGC Zones", "Gap Extents", "Preds: full, simple"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
